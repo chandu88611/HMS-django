5ef64e2cdac199f00fc6aad4549e64263a0e1dba
Chandan A S <105121265+chandu88611@users.noreply.github.com>
2024-02-16 09:23:57 +0530

    requirement  commit

--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Staff
 from .serializers import StaffSerializer
 
@@ -6,9 +7,29 @@ class StaffListCreateAPIView(generics.ListCreateAPIView):
     queryset = Staff.objects.all()
     serializer_class = StaffSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            'status': 'success',
+            'message': 'Staff list retrieved successfully',
+            'data': serializer.data
+        }
+        return Response(data)
+
 class StaffRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Staff.objects.all()
     serializer_class = StaffSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = {
+            'status': 'success',
+            'message': 'Staff details retrieved successfully',
+            'data': serializer.data
+        }
+        return Response(data)
 
 class StaffListByDepartmentAPIView(generics.ListAPIView):
     serializer_class = StaffSerializer
@@ -17,9 +38,29 @@ class StaffListByDepartmentAPIView(generics.ListAPIView):
         department = self.kwargs['department']
         return Staff.objects.filter(department=department)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            'status': 'success',
+            'message': 'Staff list retrieved successfully',
+            'data': serializer.data
+        }
+        return Response(data)
+
 class StaffListByRoleAPIView(generics.ListAPIView):
     serializer_class = StaffSerializer
 
     def get_queryset(self):
         role = self.kwargs['role']
         return Staff.objects.filter(role=role)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            'status': 'success',
+            'message': 'Staff list retrieved successfully',
+            'data': serializer.data
+        }
+        return Response(data)
